0d43279b24b5ef2c9d9cfd0e77f33bbd1e701261
Felipe Santos <72233926+Felipesco@users.noreply.github.com>
2022-09-13 07:53:32 -0300

    Update index.php

--- slim/index.php
+++ slim/index.php
@@ -7,8 +7,24 @@ require __DIR__ . ' ./vendor/autoload.php';
 
 $app = AppFactory::create();
 
+//GET  /
 $app->get('/', function (Request $request, Response $response, $args) {
-    $response->getBody()->write("Vai jacaré jacaré!");
+    $response->getBody()->write("Vai jacaré jacaré!</br>GET sem rota, entrem em locallhost:8000/oi/[seu nome]");
+    return $response;
+});
+
+
+//GET /oi
+$app->get('/oi', function (Request $request, Response $response) {
+    $response->getBody()->write("Digite locallhost:8000/oi/[seu nome] para ser mais legal");
+    return $response;
+});
+
+//GET /oi/arg
+$app->get('/oi/{nome}', function (Request $request, Response $response, array $args) {
+    $nome = $args['nome'];
+
+    $response->getBody()->write("olá, $nome</br>Utilizando metódo GET");
     return $response;
 });
 
