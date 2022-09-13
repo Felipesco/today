05e1953950cae5e00b8a3c65389a1f4002dd4098
Felipe Santos <72233926+Felipesco@users.noreply.github.com>
2022-09-13 17:08:09 -0300

    Update cineminha.py

--- py/cineminha.py
+++ py/cineminha.py
@@ -1,5 +1,6 @@
 # Um programa que mostre quantas cadeiras tem disponivel para reserva
 # Vou colocar que tem 10 cadeiras, sim... Um cinema pequeno.
+import random
 
 cadeiras = []
 cadeiras = [1,2,3,4,5,6,7,8,9,10]
@@ -7,14 +8,16 @@ cadeiras = [1,2,3,4,5,6,7,8,9,10]
 def disp():
     print("Temos " + str(len(cadeiras)) + " cadeiras disponiveis\nSão elas:"+str(cadeiras)+"\nQual deseja escolher ?")
     escolha = int(input("Cadeira: "))
-    
+    print("\n")
+
     if escolha > 10 or escolha == 0:
-        print("\nEscolha um numero de cadeira válido ")
+        print("Escolha um numero de cadeira válido!\n")
         return disp()
     
-    else:
-        # Verificação da cadeira
-        print("teste")
-
+    elif (escolha in cadeiras) == True:
+        cadeiras.remove(escolha)
+        print("Sua cadeira foi reservada com sucesso! :)\nInfo:")
+        print("Código do bilhete da cadeira: "+ random.randrange(100,250))
+        
 
 disp()
