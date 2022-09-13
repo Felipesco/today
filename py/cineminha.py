1f872257914981200a4c0309a35b248d5e9da404
Felipe Santos <72233926+Felipesco@users.noreply.github.com>
2022-09-13 17:14:29 -0300

    Update cineminha.py

--- py/cineminha.py
+++ py/cineminha.py
@@ -6,7 +6,7 @@ cadeiras = []
 cadeiras = [1,2,3,4,5,6,7,8,9,10]
 
 def disp():
-    print("Temos " + str(len(cadeiras)) + " cadeiras disponiveis\nSão elas:"+str(cadeiras)+"\nQual deseja escolher ?")
+    print("\nTemos " + str(len(cadeiras)) + " cadeiras disponiveis\nSão elas:"+str(cadeiras)+"\nQual deseja escolher ?")
     escolha = int(input("Cadeira: "))
     print("\n")
 
@@ -16,8 +16,16 @@ def disp():
     
     elif (escolha in cadeiras) == True:
         cadeiras.remove(escolha)
-        print("Sua cadeira foi reservada com sucesso! :)\nInfo:")
-        print("Código do bilhete da cadeira: "+ random.randrange(100,250))
-        
+        print("Sua cadeira foi reservada com sucesso! :)\nInfo")
+        cod = random.randrange(100,250)
+        print("Código do bilhete da cadeira: " + str(cod))
+        print("Reservar mais cadeiras?[s/n]")
+        dnv = input()
+        if dnv == "s" or dnv == "sim":
+            return disp()
+
+    else:
+        print("A cadeira " + str(escolha) + " já foi reservada\nPor Favor, escolha outra cadeira")
+        return disp()
 
 disp()
