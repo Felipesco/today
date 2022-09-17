4dc74518c654bf92208841a661a053f4be1b5dde
Felipe Santos <72233926+Felipesco@users.noreply.github.com>
2022-09-16 22:57:35 -0300

    Update teste-desenho.py

--- py/teste-desenho.py
+++ py/teste-desenho.py
@@ -2,12 +2,19 @@ import turtle
 
 seta = turtle.Turtle()
 
-for i in range(4):
-    seta.forward(100)
-    seta.left(90)
+def forma1():
+    for i in range(4):
+        seta.forward(100)
+        seta.left(90)
 
-for i in range(2):
-    seta.forward(100)
-    seta.right(60)
-    seta.forward(100)
-    seta.right(120)
+
+def forma2():
+    for i in range(10):
+        for i in range(2):
+            seta.forward(100)
+            seta.right(60)
+            seta.forward(100)
+            seta.right(120)
+        seta.right(36)
+
+forma2()
