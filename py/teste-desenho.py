b9ed0966088df124c03780526809bd1a2971db45
Felipe Santos <72233926+Felipesco@users.noreply.github.com>
2022-09-16 22:53:21 -0300

    Update teste-desenho.py

--- py/teste-desenho.py
+++ py/teste-desenho.py
@@ -2,13 +2,12 @@ import turtle
 
 seta = turtle.Turtle()
 
-seta.forward(100)
-seta.left(90)
+for i in range(4):
+    seta.forward(100)
+    seta.left(90)
 
-seta.forward(100)
-seta.left(90)
-
-seta.forward(100)
-seta.left(90)
-
-seta.forward(100)
+for i in range(2):
+    seta.forward(100)
+    seta.right(60)
+    seta.forward(100)
+    seta.right(120)
