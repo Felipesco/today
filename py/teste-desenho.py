4c53c2e4a13e682783ae87d830de4230e7c20535
Felipe Santos <72233926+Felipesco@users.noreply.github.com>
2022-09-16 23:23:23 -0300

    Update teste-desenho.py

--- py/teste-desenho.py
+++ py/teste-desenho.py
@@ -1,14 +1,22 @@
 import turtle
 
 seta = turtle.Turtle()
+seta.speed(10)
+janela = turtle.getscreen()
+
+
+turtle.Screen().bgcolor("Black")
+seta.color("cyan")
 
 def flor():
-    for i in range(10):
-        for i in range(2):
-            seta.forward(100)
-            seta.right(60)
-            seta.forward(100)
-            seta.right(120)
-        seta.right(36)
+    for i in range(4):
+        for i in range(10):
+            for i in range(2):
+                seta.forward(100)
+                seta.right(60)
+                seta.forward(100)
+                seta.right(120)
+            seta.right(36)
+        seta.right(63)
 
 flor()
